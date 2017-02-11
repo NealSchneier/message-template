92ab208d010c2498c65885baae75bfda129a3e15
Neal Schneier <neal.schneier@digitalturbine.com>
2017-02-11 14:48:46 -0500

    Add barebone doc for the template service

---
+++ src/main/scala/org/neal/schneier/service/TemplateService.scala
@@ -0,0 +1,9 @@
+package org.neal.schneier.service
+
+/**
+  * This class would own the management of the template. In an actual Real world situation it would not just
+  * be an in memory store but backed by most likely a relational database, or some other mechanism.
+  */
+class TemplateService {
+
+}
